dfb1157afdd5129e178c7f820249b1be0b3689c3
nuwasdzarrin3 <nuwasdzarrin3@gmail.com>
2023-05-01 22:08:58 +0700

    add payment

---
+++ database/migrations/2023_05_01_135135_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->string('logo')->nullable();
+            $table->enum('cost_type', ['fixed', 'percent'])->default('fixed');
+            $table->float('cost_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
