7dc6866da2bc3d8b2f9a30b34174e77a7490ddbb
nuwasdzarrin3 <nuwasdzarrin3@gmail.com>
2023-05-14 09:58:57 +0700

    update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,11 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $hidden = ['payment'];
+    protected $appends = ['payment_name'];
 
     public function event() {
         return $this->belongsTo(Event::class);
     }
+    public function payment() {
+        return $this->belongsTo(Payment::class);
+    }
     public function order_items() {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function getPaymentNameAttribute() {
+        return $this->payment ? $this->payment->name : '';
     }
 }
