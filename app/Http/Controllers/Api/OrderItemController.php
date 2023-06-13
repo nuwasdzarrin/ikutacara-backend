b04560beec4fcd3fe9a9eff7003cfec132fa5724
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2023-06-13 16:28:43 +0700

    add sort by order items

--- app/Http/Controllers/Api/OrderItemController.php
+++ app/Http/Controllers/Api/OrderItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\GeneralResponseCollection;
 use App\Models\Committee;
 use App\Models\OrderItem;
+use Illuminate\Http\Request;
 
 class OrderItemController extends Controller
 {
@@ -14,14 +15,27 @@ class OrderItemController extends Controller
         $this->middleware('auth:sanctum');
     }
 
-    public function index($event_id) {
+    public function index($event_id, Request $request) {
         $auth = auth()->user();
         $is_member = Committee::query()->where('user_id', $auth->id)->where('event_id', $event_id)
             ->first();
         if (!$is_member)
             return (new GeneralResponseCollection([], ['sorry you are not part of this event'], false))
                 ->response()->setStatusCode(400);
-        $order_items = OrderItem::query()->where('event_id', $event_id)->get();
+        $order_items = OrderItem::query()->where('event_id', $event_id);
+        if ($request->filled('sort')) {
+            $sort = explode(",", $request->sort);
+            if (count($sort) > 1) {
+                $sort_by = $sort[0];
+                $direction = $sort[1] == 'desc' ? 'desc' : 'asc';
+                if ($sort_by == 'ticket_name') $order_items = $order_items->orderBy('ticket_name', $direction);
+                else if ($sort_by == 'ticket_price')
+                    $order_items = $order_items->orderBy('ticket_price', $direction);
+                else if ($sort_by == 'created_at')
+                    $order_items = $order_items->orderBy('created_at', $direction);
+            }
+        }
+        $order_items = $order_items->get();
         return (new GeneralResponseCollection($order_items, ['Success get order item'], true))
             ->response()->setStatusCode(200);
     }
